8b8c6b1c3860d5b08d59e2e6ce2d12853a680625
Ray Walker <hello@raywalker.it>
2017-11-07 08:43:11 +1100

    Environment consolidation file.

---
+++ bin/env.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+set -ao pipefail
+
+# CONFIG FILE
+# Read parameters from key->value configuration files
+# Note this will override environment variables at this stage
+# @todo prioritise ENV over config file ?
+
+DEFAULT_CONFIG_FILE="${BUILD_DIR}/config.default"
+if [[ ! -f "${DEFAULT_CONFIG_FILE}" ]]
+then
+  fatal "Default configuration file not found: ${DEFAULT_CONFIG_FILE}"
+fi
+# shellcheck source=/dev/null
+. ${DEFAULT_CONFIG_FILE}
+
+# Read from custom config file from command line parameter
+if [[ "${CONFIG_FILE}" ]]
+then
+  if [[ ! -f "${CONFIG_FILE}" ]]
+  then
+    fatal "Custom config file not found: ${CONFIG_FILE}"
+  fi
+
+  echo "Reading config from: ${CONFIG_FILE}"
+
+  # https://github.com/koalaman/shellcheck/wiki/SC1090
+  # shellcheck source=/dev/null
+  . ${CONFIG_FILE}
+fi
+
+if [[ -z "${GITHUB_OAUTH_TOKEN}" ]]
+then
+  fatal "GITHUB_OAUTH_TOKEN environment variable not set"
+fi
+
+# Envsubst and cloudbuild.yaml variable consolidation
+APPLICATION_NAME=${APPLICATION_NAME:-${DEFAULT_APPLICATION_NAME}}
+BASE_IMAGE="${BASE_IMAGE:-${DEFAULT_BASE_IMAGE}}"
+BASE_NAMESPACE="${BASE_NAMESPACE:-${DEFAULT_BASE_NAMESPACE}}"
+BASE_TAG="${BASE_TAG:-${DEFAULT_BASE_TAG}}"
+BRANCH_NAME="${CIRCLE_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}"
+BUILD_ENVIRONMENT="${BUILD_ENVIRONMENT:-${DEFAULT_BUILD_ENVIRONMENT}}"
+BUILD_NAMESPACE="${BUILD_NAMESPACE:-${DEFAULT_BUILD_NAMESPACE}}"
+BUILD_NUM="${BUILD_NUM:-${CIRCLE_BUILD_NUM:-"local"}}"
+GIT_REF="${GIT_REF:-${DEFAULT_GIT_REF}}"
+GOOGLE_PROJECT_ID="${GOOGLE_PROJECT_ID:-${DEFAULT_GOOGLE_PROJECT_ID}}"
+IMAGE_FROM="${BASE_NAMESPACE}/${GOOGLE_PROJECT_ID}/${BASE_IMAGE}:${BASE_TAG}"
+IMAGE_MAINTAINER="${MAINTAINER:-${DEFAULT_MAINTAINER}}"
