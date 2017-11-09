db191920241c574a4e3bb8cecc2b9bd5708bbe34
Ray Walker <hello@raywalker.it>
2017-11-09 17:02:49 +1100

    Permits running env.sh from anywhere, uses get_from_tag.sh if necessary

--- bin/env.sh
+++ bin/env.sh
@@ -7,10 +7,24 @@ set -ao pipefail
 # Note this will override environment variables at this stage
 # @todo prioritise ENV over config file ?
 
-DEFAULT_CONFIG_FILE="${BUILD_DIR}/config.default"
+# Find real file path of current script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+source="${BASH_SOURCE[0]}"
+while [[ -h "$source" ]]
+do # resolve $source until the file is no longer a symlink
+ dir="$( cd -P "$( dirname "$source" )" && pwd )"
+ source="$(readlink "$source")"
+ # if $source was a relative symlink, we need to resolve it relative to the
+ # path where the symlink file was located
+ [[ $source != /* ]] && source="$dir/$source"
+done
+current_dir="$( cd -P "$( dirname "$source" )" && pwd )"
+
+DEFAULT_CONFIG_FILE="${current_dir}/../config.default"
+
 if [[ ! -f "${DEFAULT_CONFIG_FILE}" ]]
 then
-  fatal "Default configuration file not found: ${DEFAULT_CONFIG_FILE}"
+  >&2 echo "Default configuration file not found: ${DEFAULT_CONFIG_FILE}"
 fi
 # shellcheck source=/dev/null
 . ${DEFAULT_CONFIG_FILE}
@@ -20,7 +34,7 @@ if [[ "${CONFIG_FILE}" ]]
 then
   if [[ ! -f "${CONFIG_FILE}" ]]
   then
-    fatal "Custom config file not found: ${CONFIG_FILE}"
+    >&2 echo "Custom config file not found: ${CONFIG_FILE}"
   fi
 
   echo "Reading config from: ${CONFIG_FILE}"
@@ -32,18 +46,18 @@ fi
 
 if [[ -z "${GITHUB_OAUTH_TOKEN}" ]]
 then
-  fatal "GITHUB_OAUTH_TOKEN environment variable not set"
+  >&2 echo "GITHUB_OAUTH_TOKEN environment variable not set"
 fi
 
 # Envsubst and cloudbuild.yaml variable consolidation
 APPLICATION_NAME=${APPLICATION_NAME:-${DEFAULT_APPLICATION_NAME}}
-FROM_IMAGE="${FROM_IMAGE:-${DEFAULT_FROM_IMAGE}}"
-FROM_NAMESPACE="${FROM_NAMESPACE:-${DEFAULT_FROM_NAMESPACE}}"
-FROM_TAG="${FROM_TAG:-${DEFAULT_FROM_TAG}}"
 BRANCH_NAME="${CIRCLE_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}"
 BUILD_ENVIRONMENT="${BUILD_ENVIRONMENT:-${DEFAULT_BUILD_ENVIRONMENT}}"
 BUILD_NAMESPACE="${BUILD_NAMESPACE:-${DEFAULT_BUILD_NAMESPACE}}"
 BUILD_NUM="${BUILD_NUM:-${CIRCLE_BUILD_NUM:-"local"}}"
+FROM_IMAGE="${FROM_IMAGE:-${DEFAULT_FROM_IMAGE}}"
+FROM_NAMESPACE="${FROM_NAMESPACE:-${DEFAULT_FROM_NAMESPACE}}"
+FROM_TAG="${FROM_TAG:-$(${current_dir}/get_from_tag.sh)}"
 GIT_REF="${GIT_REF:-${DEFAULT_GIT_REF}}"
 GOOGLE_PROJECT_ID="${GOOGLE_PROJECT_ID:-${DEFAULT_GOOGLE_PROJECT_ID}}"
 IMAGE_MAINTAINER="${MAINTAINER:-${DEFAULT_MAINTAINER}}"
