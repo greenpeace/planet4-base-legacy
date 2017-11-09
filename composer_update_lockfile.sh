16dce8888a5cfd4d4cd99731e7b629d59ec0e5a5
Ray Walker <hello@raywalker.it>
2017-11-10 09:07:21 +1100

    Fail if GITHUB_OAUTH_TOKEN is not set.

--- composer_update_lockfile.sh
+++ composer_update_lockfile.sh
@@ -5,7 +5,7 @@ if [[ -z "${GITHUB_OAUTH_TOKEN}" ]]
 then
   >&2 echo "ERROR: \$GITHUB_OAUTH_TOKEN is not set"
   >&2 echo "       Please ensure a valid github token is available"
-  >&2 echo "       Attempting to continue ..."
+  exit 1
 fi
 
 if [[ $1 = 'dev' ]]
