1079cfe12df6d1e63d7016216451c6e81ed255a0
Ray Walker <geek@raywalker.it>
2017-11-30 16:29:10 +1100

    Simplify environment consolidation.

--- bin/env.sh
+++ bin/env.sh
@@ -27,7 +27,7 @@ then
   >&2 echo "Default configuration file not found: ${DEFAULT_CONFIG_FILE}"
 fi
 # shellcheck source=/dev/null
-. ${DEFAULT_CONFIG_FILE}
+. "${DEFAULT_CONFIG_FILE}"
 
 # Read from custom config file from command line parameter
 if [[ "${CONFIG_FILE}" ]]
@@ -41,7 +41,7 @@ then
 
   # https://github.com/koalaman/shellcheck/wiki/SC1090
   # shellcheck source=/dev/null
-  . ${CONFIG_FILE}
+  . "${CONFIG_FILE}"
 fi
 
 if [[ -z "${GITHUB_OAUTH_TOKEN}" ]]
@@ -51,18 +51,6 @@ then
 fi
 
 # Envsubst and cloudbuild.yaml variable consolidation
-APP_HOSTNAME=${APP_HOSTNAME:-${DEFAULT_APP_HOSTNAME}}
-APP_NAME=${APP_NAME:-${DEFAULT_APP_NAME}}
 BRANCH_NAME="${CIRCLE_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}"
-BUILD_ENVIRONMENT="${BUILD_ENVIRONMENT:-${DEFAULT_BUILD_ENVIRONMENT}}"
-BUILD_NAMESPACE="${BUILD_NAMESPACE:-${DEFAULT_BUILD_NAMESPACE}}"
 BUILD_NUM="${BUILD_NUM:-${CIRCLE_BUILD_NUM:-"local"}}"
 BUILD_DATE="$(date)"
-FROM_IMAGE="${FROM_IMAGE:-${DEFAULT_FROM_IMAGE}}"
-FROM_NAMESPACE="${FROM_NAMESPACE:-${DEFAULT_FROM_NAMESPACE}}"
-FROM_TAG="${FROM_TAG:-$(${current_dir}/get_from_tag.sh)}"
-GIT_REF="${GIT_REF:-${DEFAULT_GIT_REF}}"
-GOOGLE_PROJECT_ID="${GOOGLE_PROJECT_ID:-${DEFAULT_GOOGLE_PROJECT_ID}}"
-IMAGE_MAINTAINER="${MAINTAINER:-${DEFAULT_MAINTAINER}}"
-WP_EXTRA_CONFIG="${WP_EXTRA_CONFIG:-${DEFAULT_WP_EXTRA_CONFIG}}"
-WP_TITLE="${WP_TITLE:-${DEFAULT_WP_TITLE}}"
