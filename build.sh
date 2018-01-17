eac4ef7f1765587aca1b97585dba8899b98215b3
Ray Walker <geek@raywalker.it>
2018-01-17 14:27:22 +1100

    Remove DEFAULT_ vars.

--- build.sh
+++ build.sh
@@ -239,7 +239,7 @@ cloudbuild_substitutions_array=(
   "_BUILD_NUM=${BUILD_NUM}" \
   "_BRANCH_NAME=${BRANCH_NAME//[^[:alnum:]_]/-}" \
   "_COMPOSER=${COMPOSER}" \
-  "_GIT_REF=${GIT_REF:-${DEFAULT_GIT_REF}}" \
+  "_GIT_REF=${GIT_REF}" \
   "_GITHUB_OAUTH_TOKEN=${GITHUB_OAUTH_TOKEN}" \
   "_SHORT_SHA=${SHORT_SHA:-$(git rev-parse --short HEAD)}" \
 )
