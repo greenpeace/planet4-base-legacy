5f94aecd56055572e0e88260163df7e79bd36cc8
Ray Walker <hello@raywalker.it>
2018-02-09 10:40:47 +1100

    Minor optimisations in get_from_tag.sh

--- bin/get_from_tag.sh
+++ bin/get_from_tag.sh
@@ -5,22 +5,24 @@ set -eo pipefail
 #              This script attempts to determine the correct version to use
 
 
-# Find real file path of current script
-# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-source="${BASH_SOURCE[0]}"
-while [[ -h "$source" ]]
-do # resolve $source until the file is no longer a symlink
-  dir="$( cd -P "$( dirname "$source" )" && pwd )"
-  source="$(readlink "$source")"
-  # if $source was a relative symlink, we need to resolve it relative to the
-  # path where the symlink file was located
-  [[ $source != /* ]] && source="$dir/$source"
-done
-current_dir="$( cd -P "$( dirname "$source" )" && pwd )"
+
 
 # If the environment isn't already configured, source env.sh
 if [[ -z "${BUILD_NAMESPACE}" ]] || [[ -z "${GOOGLE_PROJECT_ID}" ]]
 then
+  # Find real file path of current script
+  # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+  source="${BASH_SOURCE[0]}"
+  while [[ -h "$source" ]]
+  do # resolve $source until the file is no longer a symlink
+    dir="$( cd -P "$( dirname "$source" )" && pwd )"
+    source="$(readlink "$source")"
+    # if $source was a relative symlink, we need to resolve it relative to the
+    # path where the symlink file was located
+    [[ $source != /* ]] && source="$dir/$source"
+  done
+  current_dir="$( cd -P "$( dirname "$source" )" && pwd )"
+
   # shellcheck disable=SC1090
   . "$current_dir/env.sh"
 fi
@@ -43,7 +45,7 @@ then
 fi
 
 # Search gcloud container registry for p4-onbuild images with the current branch
-if [[ "$(${gcloud_binary} container images list-tags ${BUILD_NAMESPACE}/${GOOGLE_PROJECT_ID}/p4-onbuild --filter="tags=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}" --format="table[no-heading](tags, timestamp)" 2>/dev/null)" != "" ]]
+if [[ "$(${gcloud_binary} container images list-tags "${BUILD_NAMESPACE}/${GOOGLE_PROJECT_ID}/p4-onbuild" --filter="tags=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}" --format="table[no-heading](tags, timestamp)" 2>/dev/null)" != "" ]]
 then
   >&2 echo "Found image with current branch: ${BRANCH_NAME}"
   echo "${BRANCH_NAME}"
