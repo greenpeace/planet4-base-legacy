0473a3ff09ff2d37668eefd54027382a6a7987f9
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2018-02-15 13:24:24 +0200

    removed unecessary script

--- health_db.php
+++ health_db.php
@@ -7,7 +7,6 @@ $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
 
 // Check connection
 if ($conn->connect_error) {
-	$conn->close();
 	http_response_code(503);
 	die("Connection failed: " . $conn->connect_error);
 }
